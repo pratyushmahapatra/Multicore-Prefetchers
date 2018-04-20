cdb2ebab525f240ba4e509fad7cac19fd75413e1
Suchita <Suchita@node-0.ext2compre.advosuwmadison-pg0.wisc.cloudlab.us>
2018-04-20 01:29:39 -0500

    :Revert "Add files via upload"
    This reverts commit dcab040e8cb9cdea8f97800946df50eec1f43ddc.

--- fix_whitespace.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed -i 's/[ \t]*$//' $@ # kill all trailing whitespace
-sed -i 's/\t/    /' $@  # turn each tab into 4 spaces
--- test.f
+++
@@ -1,6 +0,0 @@
-      print *, "Fortran test"
-      call zsim_roi_begin()
-      call zsim_heartbeat()
-      call zsim_roi_end()
-      print *, "Fortran test done"
-      end
